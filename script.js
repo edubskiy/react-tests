0431204071564d6e79c6f74b63383a5f0bc0c445
Evgeniy <edubskiy@openwaygroup.com>
2020-02-02 11:35:28 +0300

    Created first test file with couple of dummy tests

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ const googleDatabase = [
   'myfavouritecats2.com'
 ];
 
-const googleSearch = (searchQuery) => {
-  const matches = googleDatabase.filter(website => {
+const googleSearch = (searchQuery, db) => {
+  const matches = db.filter(website => {
     return website.includes(searchQuery);
   });
   return matches.length > 3 ? matches.slice(0, 3) : matches
 }
 
-console.log(googleSearch('cat'));
+console.log(googleSearch('cat', googleDatabase));
+
+module.exports = googleSearch;
--- script.test.js
+++ script.test.js
@@ -1,3 +1,21 @@
-it("passes", () => {
-  // implement tests
+
+const googleSearch = require('./script');
+
+const dbMock = [
+  'dog.com',
+  'cheeesepuff.com',
+  'disney.com',
+  'dogpictures.com'
+];
+
+it("is a silly test", () => {
+  expect('hello').toBe('hello');
 });
+
+it('is searching google', () => {
+  expect(googleSearch('testtest', dbMock)).toEqual([]);
+  expect(googleSearch('dog', dbMock)).toEqual([
+    'dog.com',
+    'dogpictures.com'
+  ]);
+})
