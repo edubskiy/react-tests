5215664359903a559093a10ec33102ff25ee8cd0
Evgeniy <edubskiy@openwaygroup.com>
2020-02-02 11:44:36 +0300

    Added few more tests

--- script.test.js
+++ script.test.js
@@ -19,3 +19,12 @@ it('is searching google', () => {
     'dogpictures.com'
   ]);
 })
+
+it('works with undefined and null input', () => {
+  expect(googleSearch(undefined, dbMock)).toEqual([]);
+  expect(googleSearch(null, dbMock)).toEqual([]);
+})
+
+it('does not return more than 3 matches', () => {
+  expect(googleSearch('.com', dbMock).length).toEqual(3);
+})
